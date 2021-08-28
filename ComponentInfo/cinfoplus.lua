39d505f5dc2ca72ac63d44e89a6005c6b259bf47
Käpten Wenne <80627107+KaeptenWenne@users.noreply.github.com>
2021-08-28 22:37:38 +0200

    Added filtering argument

--- ComponentInfo/cinfoplus.lua
+++ ComponentInfo/cinfoplus.lua
@@ -1,12 +1,23 @@
 local c = require("component")
+local a = { ... }
+
+if #a >= 1 and string.lower(a[1]) == "help" then
+  print("cinfoplus [component filter]")
+  print()
+  print("Example 1 (no filter):                cinfoplus")
+  print("Example 2 (component name filter):    cinfoplus me_controller")
+  print("Example 4 (component address filter): cinfoplus f07b3df0")
+end
 
 for address, type in c.list() do
-  local proxy = c.proxy(address)
-  print("== " .. string.upper(type) .. " : " .. address .. " ==")
+  if #a == 0 or (string.lower(address):find(string.lower(a[1])) ~= nil or string.lower(type):find(string.lower(a[1])) ~= nil) then
+    local proxy = c.proxy(address)
+    print("== " .. string.upper(type) .. " : " .. address .. " ==")
 
-  for a,b in pairs(proxy) do
-    print("  -> " .. a)
+    for a,b in pairs(proxy) do
+      print("  -> " .. a)
+    end
+
+    print()
   end
-
-  print()
 end
